6f1d85451619b37e0a92a7ccd4302303e7c0fe37
Luciano / NotDev <47834044+LucianoDeveloper@users.noreply.github.com>
2021-03-04 18:31:15 -0300

    NOVIDADES - LEIA README.MD

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const { images } = require("./images.json")
+const { MessageEmbed } = require("discord.js"), // ou { RichEmbed }, dependendo da sua versão do discord.js
+       fetch = require('node-fetch'),
+       url = `https://raw.githubusercontent.com/LucianoDeveloper/memes-random/master/images.json`;
+       
+     fetch(`https://raw.githubusercontent.com/LucianoDeveloper/memes-random/master/images.json`, {
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json'
+				}
+			}).then(userRes = userRes.json())
+			.then(response => {
+	    		
+     const random = Math.floor(Math.random() * response.images.length);
+     const randomMeme = response.images[random];
 
-const random = Math.floor(Math.random() * images.length)
-const randomMeme = images[random];
+      const embed = new MessageEmbed()
+       .setDescription("Meme Gerado")
+       .setImage(randomMeme);
 
-console.log(randomMeme)
+      <channel>.send(embed); // só substituir o ( <channel> ) pelo canal desejado. Por exemplo: ( message.channel.send(embed); )
+       });
